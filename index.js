0c5695523dd1d646e43325a606cca04ec89a913c
Fulvio Cusimano <fulvio.cusimano@gmail.com>
2020-11-07 10:59:25 +0100

    creating class structure

---
+++ index.js
@@ -0,0 +1,40 @@
+const Combos = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+class RomanNumber {
+  constructor(input) {
+
+  }
+
+
+  /**
+   * return an number from its roman representaion
+   * @returns {Number} number
+   */
+  toInt() {
+
+  }
+
+  /**
+   * return a roman number given a number in the range 1 - 3999
+   * @returns {String} roman representation
+   */
+  toString() {
+
+  }
+}
+
+module.exports = RomanNumber;
